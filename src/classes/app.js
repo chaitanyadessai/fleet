4f1ef31c78e2b71ca907f8df1f8ecc27195e19ef
chaitanyadessai <chaitanya.dessai@sjinnovation.com>
2021-05-29 19:56:31 +0530

    es6

---
+++ src/classes/app.js
@@ -0,0 +1,79 @@
+import {fleet} from '../fleet-data.js';
+import {FleetDataService} from '../services/fleet-data-service.js';
+
+/**
+ * The main application class.
+ */
+ export default class App {
+  
+  constructor() {
+    this.dataService = new FleetDataService();
+  }
+  
+    initData() {
+    this.dataService.loadData(fleet);
+  }
+  
+  initEvents() {
+    
+    $(".search-form [type='submit']").on('click', e => {
+      e.preventDefault();
+      let $this = $(e.currentTarget);
+      let $form = $this.closest('form');
+      let searchWhat = $form.data('search');
+      let q = $('[type="search"]', $form).val();
+      if (q != "" && searchWhat == 'drones') {
+        let drones = this.dataService.getDronesByQuery(q);
+        if (drones.length) {
+          $("#drone-list").empty();
+          this.renderDrones(drones);
+        }
+      }
+      else if (q == "" && searchWhat == 'drones') {
+        $("#drone-list").empty();
+        this.renderDrones();
+      }
+      else if (q != "" && searchWhat == 'cars') {
+        let cars = this.dataService.getCarsByQuery(q);
+        if (cars.length) {
+          $("#car-list").empty();
+          this.renderCars(cars);
+        }
+      }
+      else if (q == '' && searchWhat == 'cars') {
+        $("#car-list").empty();
+        this.renderCars();
+      }
+    });
+    
+  }
+
+
+
+renderDrones(drones = null) {
+  let context = {"drones": drones === null ? this.dataService.drones : drones};
+  let template = $("#drone-template").html();
+  let rendered = Mustache.render(template, context);
+  $("#drone-list").html(rendered);
+}
+
+renderCars(cars = null) {
+  let context = {"cars": cars === null ? this.dataService.cars : cars};
+  let template = $("#car-template").html();
+  let rendered = Mustache.render(template, context);
+  $("#car-list").html(rendered);
+}
+
+  init() {
+    this.initData();
+    this.renderDrones();
+    this.renderCars();    
+    this.initEvents();
+    console.log('App initialized!');
+  }
+  
+
+
+
+}
+
---
+++ src/services/fleet-data-service.js
@@ -0,0 +1,60 @@
+/**
+ * Manage cars and drones data.
+*/
+
+import {Car} from '../classes/car.js';
+import {Drone} from '../classes/drone.js';
+
+export class FleetDataService {
+  
+  constructor() {
+    this.drones = [];
+    this.cars = [];
+  }
+  
+  loadData(fleet) {
+    for (let item of fleet) {
+      switch (item.type) {
+        case 'drone':
+          let d = this.loadDrone(item);
+          this.drones.push(d);
+          break;
+        case 'car':
+          let c = this.loadCar(item)
+          this.cars.push(c);
+          break;
+      }
+    }
+  }
+  
+  loadCar(item) {
+    let c = new Car(item.license, item.model, item.latLng);
+    c.make = item.make;
+    c.miles = item.miles;
+    return c;
+  }
+  
+  loadDrone(item) {
+    let d = new Drone(item.license, item.model, item.latLng);
+    d.airTimeHours = item.airTimeHours;
+    d.base = item.base;
+    return d;
+  }
+  
+  getCarByLicense(license) {
+    return this.cars.find(car => car.license === license);
+  }
+  
+  getCarsSortedByLicense() {
+    return this.cars.sort( ({license: lic1}, {license: lic2}) => lic1 < lic2 ? -1 : (lic1 > lic2 ? 1 : 0) );
+  }
+  
+  getCarsByQuery(q) {
+    return this.cars.filter(car => car.license.indexOf(q) !== -1);
+  }
+  
+  getDronesByQuery(q) {
+    return this.drones.filter(drone => drone.license.indexOf(q) !== -1);
+  }
+  
+}
